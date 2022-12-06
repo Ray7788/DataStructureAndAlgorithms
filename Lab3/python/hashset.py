391f0bfd58126269903991b4a182151a7b298c67
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2022-12-06 13:01:40 +0000

    finish hash function part

--- Lab3/python/hashset.py
+++ Lab3/python/hashset.py
@@ -8,7 +8,7 @@ class hashset:
         self.mode = config.mode
         self.hash_table_size = config.init_size
         self.collision_count = 0 # count number of collisions
-        self.insert_number = 0
+        self.insert_number = 0 # count the current inserted values numbers
         self.hashtable = [None] * self.hash_table_size
 
                 
@@ -28,12 +28,39 @@ class hashset:
 
     # Functions for rehashing the hash table
     def rehash(self):
-        self.hash_table_size = self.insert_number * 2
-        new_hash_table = self.hashtable[:]
+        self.hash_table_size = self.insert_number * 2   # size of next hash table  
+        temp_hash_table = self.hashtable[:]
+        # initialise new hash table
         self.hashtable = [None] * self.hash_table_size
-        for i in range(len(new_hash_table)):
-            self.hashtable[i] = new_hash_table[i]
+        for i in range(len(temp_hash_table)):
+            self.hashtable[i] = temp_hash_table[i]
         
+    # Provide smaert choices for different mode 
+    def hashFunction(self, value):
+        if self.mode <= 3 :
+            char_count = 0
+            # Both upper and lower case 26 + 26 + 1
+            sizeASCII = 53 
+            power_ASCII = 0
+            for val in value:
+                char_count = (char_count + (ord(val) - ord('a') + 1) * power_ASCII) % self.hash_table_size
+                power_ASCII = (power_ASCII * sizeASCII) % self.hash_table_size
+            return int(char_count)
+
+        else:
+            char_count = 0
+            for val in value:
+                char_count += ord(val)
+            length = len(str(char_count))
+            if length > 3:
+                mid_int = 100 * int((str(char_count)[length // 2 - 1])) \
+                          + 10 * int((str(char_count)[length // 2])) \
+                          + 1 * int((str(char_count)[length // 2 + 1]))
+            else:
+                mid_int = char_count
+            return mid_int % self.hash_table_size
+
+
     def insert(self, value):
         # code for inserting into  hash table
 
