a4bdecb874b50b8941db6b45a975602244a847a2
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2022-12-06 19:56:12 +0000

    Correct "None"  V2

--- Lab3/python/bstree.py
+++ Lab3/python/bstree.py
@@ -5,40 +5,55 @@ class bstree:
         self.verbose = config.verbose
         self.left = None
         self.right = None
+        self.value = ""
         self.height = 0
         
     def size(self):
-        if (self.tree()):
-            return 1 + self.left.size() + self.right.size()
-        return 0
+        if self.value is not None:
+            # Check whether the left node and right node are none
+            if (self.left is not None) and (self.right is not None):
+                return 1 + self.left.size() + self.right.size()
+            elif (self.left is not None) and (self.right is None):
+                return 1 + self.left.size()
+            elif (self.left is None) and (self.right is not None):
+                return 1 + self.right.size()
+            elif (self.left is None) and (self.right is None):
+                return 1
+        else:
+            return 0
         
     def tree(self):
         # This counts as a tree if it has a field self.value
         # it should also have sub-trees self.left and self.right
-        # return hasattr(self, 'value')
-        if hasattr(self, 'value') and hasattr(self, 'left') and hasattr(self, 'right'):
-            return self.value != None and self.left != None and self.right != None
+        return hasattr(self, 'value') and hasattr(self, 'left') and hasattr(self, 'right')
         
     def insert(self, value):
-        # if tree is not NULL then insert into the correct sub-tree
-        if (self.tree()):
+        if self.tree():
+            # If the root node is None, then insert the value here
+            # if tree is not NULL then insert into the correct sub-tree
             if self.value == value:
                 return
             # If parent node's value is larger than the target value
             elif self.value > value:
-                if self.left is None:
-                    self.left = bstree(value)
+                if self.left == None:
+                    self.left = bstree()
+                    self.left.value = value
+                    return
                 else:
                     self.left.insert(value)
             # If parent node's value is smaller than the target value
             elif self.value < value:
                 if self.right is None:
-                    self.right = bstree(value)
+                    self.right = bstree()
+                    self.right.value = value
+                    return 
                 else:
                     self.right.insert(value)
         else:
             # otherwise create a new node containing the value
             self.value = value
+            self.left = None
+            self.right = None
         
     def find(self, value):
         if self.tree():
@@ -46,21 +61,22 @@ class bstree:
             if self.value == value:
                 return True
             # the value is smaller than the index value
-            if value < self.value:
-                if self.left != None:
+            if self.value > value:
+                if self.left is None:
+                    return False
+                else:
                     return self.left.find(value)
-                else:
-                    return False
             # the value is larger than than the index value
-            elif value > self.value :
-                if self.right != None:
-                    return self.right.find(value)
-                else:
+            if self.value < value:
+                if self.right is None:
                     return False
-        # if the root node is None, return False 
+                else:
+                    return self.right.find(value)
         else:
-            if self.value is None:
-                return False
+            # if the root node is None, return False 
+            return False
+        
+
         
     # You can update this if you want
     def print_set_recursive(self, depth):
@@ -97,6 +113,6 @@ class bstree:
     def print_stats(self):
         # record and print statistic
         print("Size of the tree : " + str (self.size()))
-        print("Height of the tree : " + str (self.height()))
+        # print("Height of the tree : " + str (self.height()))
             
             
