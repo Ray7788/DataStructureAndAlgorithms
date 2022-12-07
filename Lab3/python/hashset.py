f21930f7932b6efdc22fb66e586b529b52e1396a
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2022-12-07 01:07:58 +0000

    为纠错 Uncorrected file

--- Lab3/python/hashset.py
+++ Lab3/python/hashset.py
@@ -7,9 +7,11 @@ class hashset:
         self.verbose = config.verbose
         self.mode = config.mode
         self.hash_table_size = config.init_size
+
         self.collision_count = 0 # count number of collisions
         self.insert_number = 0 # count the current inserted values numbers
         self.hashtable = [None] * self.hash_table_size
+        self.load_factor = 0.45
 
                 
     # Helper functions for finding prime numbers
@@ -26,50 +28,161 @@ class hashset:
             n = n + 1
         return n
 
+    def lastPrime(self, n):
+        n = n - 1
+        while not self.isPrime(n):
+            n = n - 1
+        return n
+
     # Functions for rehashing the hash table
     def rehash(self):
         self.hash_table_size = self.insert_number * 2   # size of next hash table  
-        temp_hash_table = self.hashtable[:]
+        temp_hash_table = self.hashtable
         # initialise new hash table
         self.hashtable = [None] * self.hash_table_size
+        self.insert_number = 0
+        self.collision_count = 0
         for i in range(len(temp_hash_table)):
-            self.hashtable[i] = temp_hash_table[i]
+            self.insert(temp_hash_table[i])
         
-    # Provide smaert choices for different mode 
     def hashFunction(self, value):
-        if self.mode <= 3 :
-            char_count = 0
-            # Both upper and lower case 26 + 26 + 1
-            sizeASCII = 53 
-            power_ASCII = 0
+        hash_value = 0
+        if self.mode <= 3:
+            cValue = 51 # Prime after 26 Upper case and 26 Lower case 
             for val in value:
-                char_count = (char_count + (ord(val) - ord('a') + 1) * power_ASCII) % self.hash_table_size
-                power_ASCII = (power_ASCII * sizeASCII) % self.hash_table_size
-            return int(char_count)
-
+                hash_value = ord(val) + cValue * hash_value
         else:
-            char_count = 0
+            # a simple summation
             for val in value:
-                char_count += ord(val)
-            length = len(str(char_count))
-            if length > 3:
-                mid_int = 100 * int((str(char_count)[length // 2 - 1])) \
-                          + 10 * int((str(char_count)[length // 2])) \
-                          + 1 * int((str(char_count)[length // 2 + 1]))
-            else:
-                mid_int = char_count
-            return mid_int % self.hash_table_size
+                hash_value += ord(val)
+        return hash_value % self.hash_table_size
 
+    # Provide smaert choices for different mode 
+    def openAddressing(self, mode, hash_value, i):
+        if mode == 00: # Jump to next position
+            return (hash_value + 1) % self.hash_table_size    
+        if mode == 11: # Use quadratic probing of the form: (i^2 + i) / 2
+            hash_value = (hash_value + (i**2 + i) / 2 ) % self.hash_table_size
+            return hash_value
+        if mode == 22:  
+            h1 = hash_value % self.hash_table_size
+            h2 = hash_value % self.lastPrime(self.hash_table_size)
+            hash_value = (h1 + h2 * i) % self.hash_table_size
+            return hash_value
 
+        #     char_count = 0
+        #     # Both upper and lower case 26 + 26 + 1
+        #     sizeASCII = 53 
+        #     power_ASCII = 0
+        #     for val in value:
+        #         char_count = (char_count + (ord(val) - ord('a') + 1) * power_ASCII) % self.hash_table_size
+        #         power_ASCII = (power_ASCII * sizeASCII) % self.hash_table_size
+        #     return int(char_count)
+
+        # else:
+        #     char_count = 0
+        #     for val in value:
+        #         char_count += ord(val)
+        #     length = len(str(char_count))
+        #     if length > 3:
+        #         mid_int = 100 * int((str(char_count)[length // 2 - 1])) \
+        #                   + 10 * int((str(char_count)[length // 2])) \
+        #                   + 1 * int((str(char_count)[length // 2 + 1]))
+        #     else:
+        #         mid_int = char_count
+        #     return mid_int % self.hash_table_size
+
+    # code for inserting into hash table
     def insert(self, value):
-        # code for inserting into  hash table
+        if (self.insert_number / self.hash_table_size) > self.load_factor:
+            self.rehash()
 
-        print("Placeholder")
+        hash_value = self.hashFunction(value)
+
+        # SEPARATE_CHAINING ---------------------------------------------------------------------------------------------------
+        if self.mode == HashingModes.HASH_1_SEPARATE_CHAINING.value or self.mode == HashingModes.HASH_2_SEPARATE_CHAINING.value:
+            print("d")
         
+        # elif self.hashtable[hash_value] is None:    pass
+        elif self.hashtable[hash_value] == value:   pass    # If the value aready exist, just ignore it
+        # LINEAR_PROBING ------------------------------------------------------------------------------------------------------
+        elif self.mode == HashingModes.HASH_1_LINEAR_PROBING.value or self.mode == HashingModes.HASH_2_LINEAR_PROBING.value:
+            while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value):
+                hash_value = self.openAddressing(00, hash_value)
+                self.collision_count += 1
+            if self.hashtable[hash_value] is None:                    # If the slot is empty
+                self.hashtable[hash_value] = value
+                self.insert_number += 1
+
+        # QUADRATIC_PROBING ------------------------------------------------------------------------------------------------------
+        elif self.mode == HashingModes.HASH_1_QUADRATIC_PROBING.value or self.mode == HashingModes.HASH_2_QUADRATIC_PROBING.value:
+            i = 1
+            while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value):
+                hash_value = self.openAddressing(11, hash_value, i)
+                i += 1
+                self.collision_count += 1
+            if self.hashtable[hash_value] is None:                    # If the slot is empty
+                self.hashtable[hash_value] = value
+                self.insert_number += 1
+
+        # DOUBLE_HASHING ------------------------------------------------------------------------------------------------------
+        elif self.mode == HashingModes.HASH_1_DOUBLE_HASHING.value or self.mode == HashingModes.HASH_2_DOUBLE_HASHING.value:
+            i = 1
+            while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value):
+                hash_value = self.openAddressing(22, hash_value, i)
+                i += 1
+                self.collision_count += 1
+            if self.hashtable[hash_value] is None:                    # If the slot is empty
+                self.hashtable[hash_value] = value
+                self.insert_number += 1
+
+                
+
     def find(self, value):
-        # TODO code for looking up in hash table
-        print("Placeholder")
-        
+        hash_value = self.hashFunc(value)
+        ini_hash_value = hash_value
+
+        # SEPARATE_CHAINING ---------------------------------------------------------------------------------------------------
+        if self.mode == HashingModes.HASH_1_SEPARATE_CHAINING.value or self.mode == HashingModes.HASH_2_SEPARATE_CHAINING.value:
+            print("d")
+
+        elif self.hashtable[hash_value] is None:   
+            return False
+        elif self.hashtable[hash_value] == value:       # If the value aready exist, just ignore it
+            return True
+
+        # LINEAR_PROBING ------------------------------------------------------------------------------------------------------
+        elif self.mode == HashingModes.HASH_1_LINEAR_PROBING.value or self.mode == HashingModes.HASH_2_LINEAR_PROBING.value:
+            while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value):
+                hash_value = self.openAddressing(00, hash_value)
+                if hash_value == ini_hash_value:
+                    return False
+
+
+        # QUADRATIC_PROBING ------------------------------------------------------------------------------------------------------
+        elif self.mode == HashingModes.HASH_1_QUADRATIC_PROBING.value or self.mode == HashingModes.HASH_2_QUADRATIC_PROBING.value:
+            i = 1
+            while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value): 
+                i += 1
+                hash_value = self.openAddressing(11, hash_value, i)
+                if hash_value == ini_hash_value:
+                    return False
+
+        # DOUBLE_HASHING ------------------------------------------------------------------------------------------------------
+        elif self.mode == HashingModes.HASH_1_DOUBLE_HASHING.value or self.mode == HashingModes.HASH_2_DOUBLE_HASHING.value:
+            i = 1
+            while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value):
+                i += 1
+                hash_value = self.openAddressing(22, hash_value, i)
+                if hash_value == ini_hash_value:
+                   return False
+
+
+        if self.hashtable[hash_value] is None:
+            return False
+        elif self.hashtable[hash_value] == value:
+            return True
+
     def print_set(self):
         # TODO code for printing hash table
         print("Placeholder")
