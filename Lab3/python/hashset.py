2fe5a332b8b1744f30a98c33368a815ed6813025
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2022-12-07 14:58:31 +0000

    完成前3种mode，可在simple运行（不含separate），初始化cell

--- Lab3/python/hashset.py
+++ Lab3/python/hashset.py
@@ -10,6 +10,9 @@ class hashset:
 
         self.collision_count = 0 # count number of collisions
         self.insert_number = 0 # count the current inserted values numbers
+
+        # for i in range(self.hash_table_size):
+        #     self.hashtable = [cell()] * self.hash_table_size
         self.hashtable = [None] * self.hash_table_size
         self.load_factor = 0.45
 
@@ -57,19 +60,21 @@ class hashset:
                 hash_value += ord(val)
         return hash_value % self.hash_table_size
 
-    # Provide smaert choices for different mode 
-    def openAddressing(self, mode, hash_value, i):
-        if mode == 00: # Jump to next position
-            return (hash_value + 1) % self.hash_table_size    
-        if mode == 11: # Use quadratic probing of the form: (i^2 + i) / 2
-            hash_value = (hash_value + (i**2 + i) / 2 ) % self.hash_table_size
-            return hash_value
-        if mode == 22:  
-            h1 = hash_value % self.hash_table_size
-            h2 = hash_value % self.lastPrime(self.hash_table_size)
-            hash_value = (h1 + h2 * i) % self.hash_table_size
-            return hash_value
+    # Provide smart choices for different mode 
+    def openAddressing_LINEAR_PROBING(self, hash_value):
+        # Jump to next position
+        return (hash_value + 1) % self.hash_table_size
 
+    def openAddressing_QUADRATIC_PROBING(self, hash_value, i):
+        # Use quadratic probing of the form: (i^2 + i) / 2
+        return (hash_value + (i**2)) % self.hash_table_size
+
+    def openAddressing_DOUBLE_HASHING(self, hash_value, i):
+        h1 = hash_value % self.hash_table_size
+        h2 = hash_value % self.lastPrime(self.hash_table_size)
+        hash_value = (h1 + h2 * i) % self.hash_table_size
+        return hash_value
+        
         #     char_count = 0
         #     # Both upper and lower case 26 + 26 + 1
         #     sizeASCII = 53 
@@ -78,19 +83,6 @@ class hashset:
         #         char_count = (char_count + (ord(val) - ord('a') + 1) * power_ASCII) % self.hash_table_size
         #         power_ASCII = (power_ASCII * sizeASCII) % self.hash_table_size
         #     return int(char_count)
-
-        # else:
-        #     char_count = 0
-        #     for val in value:
-        #         char_count += ord(val)
-        #     length = len(str(char_count))
-        #     if length > 3:
-        #         mid_int = 100 * int((str(char_count)[length // 2 - 1])) \
-        #                   + 10 * int((str(char_count)[length // 2])) \
-        #                   + 1 * int((str(char_count)[length // 2 + 1]))
-        #     else:
-        #         mid_int = char_count
-        #     return mid_int % self.hash_table_size
 
     # code for inserting into hash table
     def insert(self, value):
@@ -103,12 +95,11 @@ class hashset:
         if self.mode == HashingModes.HASH_1_SEPARATE_CHAINING.value or self.mode == HashingModes.HASH_2_SEPARATE_CHAINING.value:
             pass
         
-        # elif self.hashtable[hash_value] is None:    pass
         elif self.hashtable[hash_value] == value:   pass    # If the value aready exist, just ignore it
         # LINEAR_PROBING ------------------------------------------------------------------------------------------------------
         elif self.mode == HashingModes.HASH_1_LINEAR_PROBING.value or self.mode == HashingModes.HASH_2_LINEAR_PROBING.value:
             while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value):
-                hash_value = self.openAddressing(00, hash_value)
+                hash_value = self.openAddressing_LINEAR_PROBING(hash_value)
                 self.collision_count += 1
             if self.hashtable[hash_value] is None:                    # If the slot is empty
                 self.hashtable[hash_value] = value
@@ -118,7 +109,7 @@ class hashset:
         elif self.mode == HashingModes.HASH_1_QUADRATIC_PROBING.value or self.mode == HashingModes.HASH_2_QUADRATIC_PROBING.value:
             i = 1
             while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value):
-                hash_value = self.openAddressing(11, hash_value, i)
+                hash_value = self.openAddressing_QUADRATIC_PROBING(hash_value, i)
                 i += 1
                 self.collision_count += 1
             if self.hashtable[hash_value] is None:                    # If the slot is empty
@@ -129,18 +120,16 @@ class hashset:
         elif self.mode == HashingModes.HASH_1_DOUBLE_HASHING.value or self.mode == HashingModes.HASH_2_DOUBLE_HASHING.value:
             i = 1
             while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value):
-                hash_value = self.openAddressing(22, hash_value, i)
+                hash_value = self.openAddressing_DOUBLE_HASHING(hash_value, i)
                 i += 1
                 self.collision_count += 1
             if self.hashtable[hash_value] is None:                    # If the slot is empty
                 self.hashtable[hash_value] = value
                 self.insert_number += 1
 
-                
-
     def find(self, value):
         hash_value = self.hashFunction(value)
-        ini_hash_value = hash_value
+        ini_hash_value = hash_value # back up
 
         # SEPARATE_CHAINING ---------------------------------------------------------------------------------------------------
         if self.mode == HashingModes.HASH_1_SEPARATE_CHAINING.value or self.mode == HashingModes.HASH_2_SEPARATE_CHAINING.value:
@@ -154,17 +143,16 @@ class hashset:
         # LINEAR_PROBING ------------------------------------------------------------------------------------------------------
         elif self.mode == HashingModes.HASH_1_LINEAR_PROBING.value or self.mode == HashingModes.HASH_2_LINEAR_PROBING.value:
             while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value):
-                hash_value = self.openAddressing(00, hash_value)
+                hash_value = self.openAddressing_LINEAR_PROBING(hash_value)
                 if hash_value == ini_hash_value:
                     return False
-
 
         # QUADRATIC_PROBING ------------------------------------------------------------------------------------------------------
         elif self.mode == HashingModes.HASH_1_QUADRATIC_PROBING.value or self.mode == HashingModes.HASH_2_QUADRATIC_PROBING.value:
             i = 1
             while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value): 
                 i += 1
-                hash_value = self.openAddressing(11, hash_value, i)
+                hash_value = self.openAddressing_QUADRATIC_PROBING(hash_value, i)
                 if hash_value == ini_hash_value:
                     return False
 
@@ -173,15 +161,9 @@ class hashset:
             i = 1
             while (self.hashtable[hash_value] is not None) and (self.hashtable[hash_value] != value):
                 i += 1
-                hash_value = self.openAddressing(22, hash_value, i)
+                hash_value = self.openAddressing_DOUBLE_HASHING(22, hash_value, i)
                 if hash_value == ini_hash_value:
                    return False
-
-
-        if self.hashtable[hash_value] is None:
-            return False
-        elif self.hashtable[hash_value] == value:
-            return True
 
     def print_set(self):
         # TODO code for printing hash table
@@ -196,7 +178,21 @@ class hashset:
 # You will need alternative data-structures for separate chaining
 class cell:
     def __init__(self):
-        pass
+        self.state = 0 # state attribute 0 
+        self.word = "" #  string word looking for 
+
+    # getter & setter
+    def get_state(self):
+        return self.state
+
+    def set_state(self, num):
+        self.state = num
+
+    def get_word(self):
+        return self.word
+
+    def set_word(self, word):
+        self.word = word
         
 class state(Enum):
     empty = 0
