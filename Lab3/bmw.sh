1b90a43760e99806440706403b5c976af7caeadf
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2022-12-08 08:48:22 +0000

    改正部分文件路径

--- Lab3/bmw.sh
+++ Lab3/bmw.sh
@@ -4,13 +4,13 @@ SIZES="10000 20000 30000 40000 50000 60000 70000"
 
 IFS='', read -r -a ASIZES1 <<< "$ASIZES" 
 
-rm data2/sorted_data_python.dat 
-rm data2 reverse_data_python.dat 
-rm data2/random_data_python.dat 
+# rm data_output/sorted_data_python.dat 
+# rm data_output/reverse_data_python.dat 
+# rm data_output/random_data_python.dat 
 
-rm data2/sorted_data_python.csv 
-rm data2/reverse_data_python.csv
-rm data2/random_data_python.csv 
+# rm data_output/sorted_data_python.csv 
+# rm data_output/reverse_data_python.csv
+# rm data_output/random_data_python.csv 
 
 for STATE in $STATES 
 do
@@ -26,7 +26,7 @@ do
         ts="${ASIZES1[2]}"
     elif [ $SIZE == "40000" ]; then
         ts="${ASIZES1[3]}"
-    elif [ $SIZE == "50000"]; then
+    elif [ $SIZE == "50000" ]; then
         ts="${ASIZES1[4]}"
     elif [ $SIZE == "60000" ]; then
         ts="${ASIZES1[5]}" 
@@ -38,13 +38,13 @@ do
 for COUNT in 1 2 3 4 5
 do
 
-    ALL_TIME=`(time -p python3 ../../python/speller_darray.py -d ../data2/dict_${SIZE}_${STATE}_$COUNT -m 1 -s ${SIZE} ../dictionaries_and_queries/query_${SIZE}_${STATE}_$COUNT) 2>&1 | grep -E "user|sys" | sed s/[a-z]//g`
+    ALL_TIME=`(time -p python3 ./python/speller_darray.py -d ./test_data/test_dict/dict_${SIZE}_${STATE}_$COUNT -s ${SIZE} -m 0 ./test_data/test_query/query_${SIZE}_${STATE}_$COUNT) 2>&1 | grep -E "user|sys" | sed s/[a-z]//g`
 
     RUNTIME=`echo ${ALL_TIME[1]} + ${ALL_TIME[2]} | bc`
     COLL=`echo ${ALL_TIME[O]}`
 
-echo $SIZE $RUNTIME, $COLL >> data/${STATE}_data2_python.dat 
-echo $SIZE, $RUNTIME, $COLL >> data/${STATE}_data2_python.csv 
+echo $SIZE, $RUNTIME, $COLL >> data_output/${STATE}_data_output_python.dat 
+echo $SIZE, $RUNTIME, $COLL >> data_output/${STATE}_data_output_python.csv 
 
 done 
 
