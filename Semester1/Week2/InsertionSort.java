93375e983ed52e674dc77c81aec21fbb3e5c0d70
Ray7788 <ray778@foxmail.com>
2023-01-02 10:52:10 +0000

    2 补充复杂度

--- Semester1/Week2/InsertionSort.java
+++ Semester1/Week2/InsertionSort.java
@@ -1,6 +1,10 @@
 package Semester1.Week2;
 
 // O(n²) 是插入排序！！ 不是查找！
+// In other words, the book is saying "the worst case running time of insertion sort is at least cn²" i.e, in the worst case it would not run better than cn².
+// (Similarly, worst case is O(n²) means that the worst running time is at most c2 n², ie. would not run worse than c2 n²
+// in which, since the worst case is O(n²) and Ω(n²), the strict bound is n², denoted as θ(n²))
+
 public class InsertionSort {
     public void search(int[] A, int n){
         for(int i = 2; i < n; i++){
