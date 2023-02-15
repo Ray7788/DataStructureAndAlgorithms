55148377bb95abdcf8e50b173f5d8a2ab2607aac
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-02-15 22:14:46 +0000

    正确版本脚本（可存储）

---
+++ lab5/kp_output.sh
@@ -0,0 +1,40 @@
+CAPACITYS="5000 6000 7000 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000 110000 120000 130000 140000 150000 160000 170000 180000 190000 200000"
+
+## initialise a folder which saves raw txt file
+mkdir ./data/data3b
+mkdir ./data/time3b
+## clear and initialise csv and dat
+rm ./data/time3b/time.csv
+rm ./data/time3b/time.dat
+
+touch ./data/time3b/time.csv
+touch ./data/time3b/time.dat
+
+for CAPACITY in $CAPACITYS
+do
+
+## special cases may use this
+# for COUNT in 1 2 3 4 5
+# do
+
+## number of items you want to put in the knapsack ｜ capacity of the knapsack ｜ upper bound on the profit and weight of each item ｜ name of the output file
+    python3 kp_generate.py 300 ${CAPACITY} 5000 ./data/data3b/${CAPACITY}.txt 
+
+## record time
+    ALL_TIME=`(time -p python3 ./python/dp_kp.py ./data/data3b/${CAPACITY}.txt) 2>&1 | grep -E "user|sys" | sed s/[a-z]//g`
+    RUNTIME=0
+    for i in $ALL_TIME;
+    do RUNTIME=`echo $RUNTIME + $i|bc`;
+    done
+    echo $CAPACITY, $RUNTIME >> ./data/time3b/time.csv
+    echo $CAPACITY $RUNTIME >> ./data/time3b/time.dat
+
+# done 
+
+done
+
+
+# This script is written by Rui Xu, Feb 2023.
+# Two main functions: 
+# 1.Use kp_generate.py to generate source data by a different capacity for dp_kp.py 
+# 2. record and save the execution time 
